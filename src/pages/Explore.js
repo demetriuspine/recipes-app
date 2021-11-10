3225acdb42db44e8b8a498a4f6a7cd7a7b0160d3
lucas <mr.pine97@gmail.com>
2021-11-10 16:58:37 -0300

    impl: explore, exploreDrinks e exploreMeals

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 
+const EXPLORE_FOOD = 'explore-food';
+const EXPLORE_DRINKS = 'explore-drinks';
+
 function Explorar() {
+  const history = useHistory();
   return (
     <>
       <header>
         <Header title="Explorar" search={ false } />
       </header>
+      <button
+        type="button"
+        data-testid={ EXPLORE_FOOD }
+        onClick={ () => history.push('/explorar/comidas') }
+      >
+        Explorar Comidas
+      </button>
+      <button
+        type="button"
+        data-testid={ EXPLORE_DRINKS }
+        onClick={ () => history.push('/explorar/bebidas') }
+      >
+        Explorar Comidas
+      </button>
       <Footer />
     </>
   );
