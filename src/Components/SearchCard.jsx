e506e62ef821cd96dd0ad4d40c90c712e4cf1336
Thales Lima <thales.bplima@gmail.com>
2021-11-11 18:00:19 -0300

    tryfix component meals + req25

--- src/Components/SearchCard.jsx
+++ src/Components/SearchCard.jsx
@@ -2,22 +2,22 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function SearchCard({ picture, name, id, meals }) {
+export default function SearchCard({ picture, name, id, meals, index }) {
   const linkToMeals = (
-    <Link to={ `/comidas/${id}` }>
+    <Link to={ `/comidas/${id}` } data-testid={ `${index}-card-name` }>
       {name}
     </Link>
   );
 
   const linkToDrink = (
-    <Link to={ `/bebidas/${id}` }>
+    <Link to={ `/bebidas/${id}` } data-testid={ `${index}-card-name` }>
       {name}
     </Link>
   );
 
   return (
-    <section>
-      <img src={ picture } alt={ name } />
+    <section data-testid={ `${index}-recipe-card` }>
+      <img src={ picture } alt={ name } data-testid={ `${index}-card-img` } />
       { meals ? linkToMeals : linkToDrink }
     </section>
   );
@@ -28,6 +28,7 @@ SearchCard.propTypes = {
   meals: PropTypes.bool.isRequired,
   name: PropTypes.string.isRequired,
   picture: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 // esse card irá renderizar nome e foto do drink
--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -4,14 +4,27 @@ import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 import SearchCard from '../Components/SearchCard';
 
-function Meals() {
+function Meals(/* { history } */) {
   const [mealSearch, setMealSearch] = useState([]);
+  /* const [submitted, setSubmitted] = useState(false); */
   const resultsFromGlobalState = useSelector((state) => (
     state.searchReducer.results));
 
   useEffect(() => {
     setMealSearch(resultsFromGlobalState);
+    setSubmitted(true);
   }, [resultsFromGlobalState]);
+
+  /*   useEffect(() => {
+    if (!mealSearch) {
+      global.alert(
+        'Sinto muito, não encontramos nenhuma receita para esses filtros.',
+      );
+    } else if (submitted === true && mealSearch.meals.length === 1) {
+      history.push(`/comidas/${mealSearch[0].idMeal}`);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }); */
 
   return (
     <section>
@@ -19,12 +32,13 @@ function Meals() {
         <Header title="Comidas" search meals />
       </header>
       { mealSearch.length === 0 ? ''
-        : mealSearch.meals.map(({ strMeal, strMealThumb, idMeal }) => (
+        : mealSearch.meals.map(({ strMeal, strMealThumb, idMeal }, index) => (
           <SearchCard
             key={ idMeal }
             picture={ strMealThumb }
             name={ strMeal }
             id={ idMeal }
+            index={ index }
             meals
           />
         )) }
