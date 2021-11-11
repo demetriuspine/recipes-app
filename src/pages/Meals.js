73080460cd1f189462c03787d65a98884dde4ebf
Thales Lima <thales.bplima@gmail.com>
2021-11-11 18:29:56 -0300

    fix proptypes on component meals

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
