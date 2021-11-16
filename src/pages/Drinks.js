65d8370b91d436ec3b5e58239052c8d5dd9f490a
lucas <mr.pine97@gmail.com>
2021-11-16 18:38:50 -0300

    feat: adiciona 12 primeiras receitas nas telas de comidas e bebidas

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,17 +1,33 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 import SearchCard from '../Components/SearchCard';
+import { fetchDrinksNameAPI } from '../services/drinksAPI';
 
 const TWELVE = 12;
 
 function Drinks({ history }) {
   const [drinkSearch, setDrinkSearch] = useState([]);
+  const [isFetched, setIsFetched] = useState(false);
+  const [initialRecipes, setInitialRecipes] = useState([]);
   const [submitted, setSubmitted] = useState(false);
   const resultsFromGlobalState = useSelector((state) => (
     state.searchReducer.results));
+
+  useEffect(async () => {
+    setIsFetched(false);
+    async function getInitialRecipes() {
+      const recipes = await fetchDrinksNameAPI('');
+      setInitialRecipes(recipes);
+    }
+    await getInitialRecipes();
+    setIsFetched(true);
+    // setInitialRecipes()
+  }, []);
 
   useEffect(() => {
     setDrinkSearch(resultsFromGlobalState);
@@ -25,24 +41,38 @@ function Drinks({ history }) {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [drinkSearch]);
   return (
-    <>
-      <header>
-        <Header title="Bebidas" search meals={ false } type="drinks" />
-      </header>
-      { drinkSearch.length === 0 || !drinkSearch.drinks ? ''
-        : drinkSearch.drinks.filter((_, idx) => idx < TWELVE)
-          .map(({ strDrink, strDrinkThumb, idDrink }, index) => (
-            <SearchCard
-              key={ idDrink }
-              picture={ strDrinkThumb }
-              name={ strDrink }
-              id={ idDrink }
-              index={ index }
-              meals={ false }
-            />
-          )) }
-      <Footer />
-    </>
+    isFetched ? (
+      <>
+        <header>
+          <Header title="Bebidas" search meals={ false } type="drinks" />
+        </header>
+        { drinkSearch.length === 0 || !drinkSearch.drinks
+          ? initialRecipes.drinks.filter((_, idx) => idx < TWELVE)
+            .map(({ strDrink, strDrinkThumb, idDrink }, index) => (
+              <SearchCard
+                key={ idDrink }
+                picture={ strDrinkThumb }
+                name={ strDrink }
+                id={ idDrink }
+                index={ index }
+                meals={ false }
+                data-testid={ `${index}-recipe-card` }
+              />
+            ))
+          : drinkSearch.drinks.filter((_, idx) => idx < TWELVE)
+            .map(({ strDrink, strDrinkThumb, idDrink }, index) => (
+              <SearchCard
+                key={ idDrink }
+                picture={ strDrinkThumb }
+                name={ strDrink }
+                id={ idDrink }
+                index={ index }
+                meals={ false }
+              />
+            )) }
+        <Footer />
+      </>
+    ) : 'loading'
   );
 }
 
--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,17 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 import SearchCard from '../Components/SearchCard';
+import { fetchNameAPI } from '../services/mealsAPI';
 
 const TWELVE = 12;
 
 function Meals({ history }) {
   const [mealSearch, setMealSearch] = useState([]);
+  const [isFetched, setIsFetched] = useState(false);
   const [submitted, setSubmitted] = useState(false);
+  const [initialRecipes, setInitialRecipes] = useState([]);
   const resultsFromGlobalState = useSelector((state) => (
     state.searchReducer.results));
+
+  useEffect(async () => {
+    setIsFetched(false);
+    async function getInitialRecipes() {
+      const recipes = await fetchNameAPI('');
+      setInitialRecipes(recipes);
+    }
+    await getInitialRecipes();
+    setIsFetched(true);
+    // setInitialRecipes()
+  }, []);
 
   useEffect(() => {
     setMealSearch(resultsFromGlobalState);
@@ -26,24 +41,38 @@ function Meals({ history }) {
   }, [mealSearch]);
 
   return (
-    <section>
-      <header>
-        <Header title="Comidas" search meals type="meals" />
-      </header>
-      { mealSearch.length === 0 || !mealSearch.meals ? ''
-        : mealSearch.meals.filter((_, idx) => idx < TWELVE)
-          .map(({ strMeal, strMealThumb, idMeal }, index) => (
-            <SearchCard
-              key={ idMeal }
-              picture={ strMealThumb }
-              name={ strMeal }
-              id={ idMeal }
-              index={ index }
-              meals
-            />
-          )) }
-      <Footer />
-    </section>
+    isFetched ? (
+      <section>
+        <header>
+          <Header title="Comidas" search meals type="meals" />
+        </header>
+        { mealSearch.length === 0 || !mealSearch.meals
+          ? initialRecipes.meals.filter((_, idx) => idx < TWELVE)
+            .map(({ strMeal, strMealThumb, idMeal }, index) => (
+              <SearchCard
+                key={ idMeal }
+                picture={ strMealThumb }
+                name={ strMeal }
+                id={ idMeal }
+                index={ index }
+                meals
+                data-testid={ `${index}-recipe-card` }
+              />
+            ))
+          : mealSearch.meals.filter((_, idx) => idx < TWELVE)
+            .map(({ strMeal, strMealThumb, idMeal }, index) => (
+              <SearchCard
+                key={ idMeal }
+                picture={ strMealThumb }
+                name={ strMeal }
+                id={ idMeal }
+                index={ index }
+                meals
+              />
+            )) }
+        <Footer />
+      </section>
+    ) : ''
   );
 }
 
