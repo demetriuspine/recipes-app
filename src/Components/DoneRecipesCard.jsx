4af50df05b51ec4357fa1c47db19fede53d40da2
Thales Lima <thales.bplima@gmail.com>
2021-11-18 19:00:33 -0300

    impl reqs 54, 55, 56, 58, 59

---
+++ src/Components/DoneRecipesCard.jsx
@@ -0,0 +1,81 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
+
+function DoneRecipesCard({ recipe: {
+  id,
+  name,
+  image,
+  category,
+  tags,
+  doneDate,
+  area,
+  alcoholicOrNot,
+  type,
+}, index }) {
+  const mealInfo = (
+    <div>
+      <p data-testid={ `${index}-horizontal-top-text` }>{ `${area} - ${category}` }</p>
+      <div>
+        { tags.map((tag, idx) => (
+          <p
+            key={ idx }
+            data-testid={ `${index}-${tag}-horizontal-tag` }
+          >
+            { tag }
+          </p>
+        )) }
+      </div>
+    </div>
+  );
+
+  return (
+    <section>
+      <Link to={ `/${type}s/${id}` }>
+        <img
+          data-testid={ `${index}-horizontal-image` }
+          src={ image }
+          alt={ name }
+          width="200"
+          height="200"
+        />
+        <p data-testid={ `${index}-horizontal-name` }>{ name }</p>
+      </Link>
+      <button
+        type="button"
+      >
+        <img
+          src={ shareIcon }
+          alt="Share"
+          data-testid={ `${index}-horizontal-share-btn` }
+        />
+      </button>
+      { type === 'comida'
+        ? mealInfo
+        : <p data-testid={ `${index}-horizontal-top-text` }>{ alcoholicOrNot }</p> }
+      <p data-testid={ `${index}-horizontal-done-date` }>{ doneDate }</p>
+    </section>
+  );
+}
+
+DoneRecipesCard.propTypes = {
+  recipe: PropTypes.arrayOf(PropTypes.any).isRequired,
+  category: PropTypes.string,
+  doneDate: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  tags: PropTypes.arrayOf(PropTypes.any).isRequired,
+  area: PropTypes.string,
+  alcoholicOrNot: PropTypes.string,
+  type: PropTypes.string.isRequired,
+};
+
+DoneRecipesCard.defaultProps = {
+  alcoholicOrNot: '',
+  area: '',
+  category: '',
+};
+
+export default DoneRecipesCard;
--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,11 +1,95 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../Components/Header';
+import DoneRecipesCard from '../Components/DoneRecipesCard';
+
+const mockedStorage = [
+  {
+    id: '52771',
+    type: 'comida',
+    area: 'Italian',
+    category: 'Vegetarian',
+    alcoholicOrNot: '',
+    name: 'Spicy Arrabiata Penne',
+    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+    doneDate: '23/06/2020',
+    tags: ['Pasta', 'Curry'],
+  },
+  {
+    id: '178319',
+    type: 'bebida',
+    area: '',
+    category: 'Cocktail',
+    alcoholicOrNot: 'Alcoholic',
+    name: 'Aquamarine',
+    image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+    doneDate: '23/06/2020',
+    tags: [],
+  },
+];
 
 function DoneRecipes() {
+  const [doneRecipes, setDoneRecipes] = useState([]);
+  const [filteredRecipes, setFilteredRecipes] = useState('');
+
+  localStorage.setItem('doneRecipes', JSON.stringify(mockedStorage));
+
+  function getdoneRecipesFromStorage() {
+    return JSON.parse(localStorage.getItem('doneRecipes'));
+  }
+
+  useEffect(() => {
+    setDoneRecipes(getdoneRecipesFromStorage());
+  }, []);
+
+  function filterDoneRecipes(allDoneRecipes) {
+    return (
+      allDoneRecipes
+        .filter((recipe) => (filteredRecipes !== ''
+          ? recipe.type === filteredRecipes
+          : allDoneRecipes))
+    );
+  }
+
   return (
-    <header>
-      <Header title="Receitas Feitas" search={ false } />
-    </header>
+    <main>
+      <header>
+        <Header title="Receitas Feitas" search={ false } />
+      </header>
+      <section>
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+          onClick={ () => setFilteredRecipes('') }
+        >
+          All
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-food-btn"
+          onClick={ () => setFilteredRecipes('comida') }
+        >
+          Foods
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+          onClick={ () => setFilteredRecipes('bebida') }
+        >
+          Drinks
+        </button>
+      </section>
+      <section>
+        { doneRecipes.length !== 0
+          ? filterDoneRecipes(doneRecipes).map((recipe, index) => (
+            <DoneRecipesCard
+              key={ index }
+              recipe={ recipe }
+              index={ index }
+            />
+          ))
+          : ''}
+      </section>
+    </main>
   );
 }
 
