24cc0043555d2a4455a8161baecc8057817068c9
Haron Prestes <haronprestes1303@gmail.com>
2021-11-19 15:08:40 -0300

    Requisitos 47 48 e 49

--- src/pages/MealsInProgress.js
+++ src/pages/MealsInProgress.js
@@ -1,10 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import CheckboxIngredient from '../Components/CheckboxIngrendient';
 
 function MealsInProgress() {
+  const url = (index) => `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${index}`;
+  const [recipe, setRecipe] = useState([]);
+  const history = useHistory();
+  const test = 52771;
+  useEffect(() => {
+    async function getRecipe() {
+      await fetch(url(test))
+        .then((response) => response.json())
+        .then((data) => setRecipe(data.meals));
+    }
+    getRecipe();
+  }, []);
+  function returnValue(a, b, string) {
+    if (a.toLowerCase().includes(string) && b) {
+      return b;
+    }
+    return undefined;
+  }
+
+  const entries = recipe[0] ? Object.entries(recipe[0]) : [];
+  const ingredients = entries.map((e) => returnValue(e[0], e[1], 'ingredient'))
+    .filter((e) => e !== undefined);
+  const measures = entries.map((e) => returnValue(e[0], e[1], 'measure'))
+    .filter((e) => e !== undefined);
   return (
-    <section>
-      <p>MealsInProgress</p>
-    </section>
+    <>
+      { recipe.map((rec) => (
+        <main key={ rec.idMeal }>
+          <h2 data-testid="recipe-title">
+            {rec.strMeal}
+          </h2>
+          <img
+            src={ rec.strMealThumb }
+            data-testid="recipe-photo"
+            alt="foto de receita"
+          />
+          <img
+            src={ shareIcon }
+            data-testid="share-btn"
+            alt="botão de compartilhar"
+          />
+          <img
+            src={ whiteHeartIcon }
+            data-testid="favorite-btn"
+            alt="botão de faovritar"
+          />
+          <p data-testid="recipe-category">
+            { rec.strCategory }
+          </p>
+          <p data-testid="instructions">
+            {rec.strInstructions}
+          </p>
+          <button
+            type="button"
+            data-testid="finish-recipe-btn"
+            onClick={ () => history.push('/receitas-feitas') }
+          >
+            Finalizar Receita
+          </button>
+        </main>
+      ))}
+      { ingredients.map((ingredient, index) => (
+        <CheckboxIngredient
+          key={ index }
+          index={ index }
+          ingredient={ ingredient }
+          measure={ measures[index] }
+        />))}
+    </>
   );
 }
 
