8e00840a39e99448c9d7884e2d73b29813fb4cce
Thales Lima <thales.bplima@gmail.com>
2021-11-19 15:05:26 -0300

    hotfix done-recipes

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -2,39 +2,14 @@ import React, { useEffect, useState } from 'react';
 import DoneRecipesCard from '../Components/DoneRecipesCard';
 import Header from '../Components/Header';
 
-const mockedStorage = [
-  {
-    id: '52771',
-    type: 'comida',
-    area: 'Italian',
-    category: 'Vegetarian',
-    alcoholicOrNot: '',
-    name: 'Spicy Arrabiata Penne',
-    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-    doneDate: '23/06/2020',
-    tags: ['Pasta', 'Curry'],
-  },
-  {
-    id: '178319',
-    type: 'bebida',
-    area: '',
-    category: 'Cocktail',
-    alcoholicOrNot: 'Alcoholic',
-    name: 'Aquamarine',
-    image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-    doneDate: '23/06/2020',
-    tags: [],
-  },
-];
-
 function DoneRecipes() {
   const [doneRecipes, setDoneRecipes] = useState([]);
   const [filteredRecipes, setFilteredRecipes] = useState('');
 
-  localStorage.setItem('doneRecipes', JSON.stringify(mockedStorage));
-
   function getdoneRecipesFromStorage() {
-    return JSON.parse(localStorage.getItem('doneRecipes'));
+    if (localStorage.getItem('doneRecipes')) {
+      return JSON.parse(localStorage.getItem('doneRecipes'));
+    }
   }
 
   useEffect(() => {
